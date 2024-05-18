19b5c804b4521be7932d9be2870a673677140765
polubarev <i.polubarev@gmail.com>
2024-05-18 23:18:09 +0300

    removed secrets, added using env vars

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Telegram bot for chat summary
+# TODO:
+- manage not only text messages, but images and other ones
+- add summary chat gpt
--- app.py
+++ app.py
@@ -1,34 +1,62 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 import logging
+import json
+from datetime import datetime
+import os
+
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Token provided by BotFather on Telegram
-TOKEN = '6835758891:AAEaqo2y77vGDF0gIQo85wQoLMZQWyGqbXg'
+GIT_TOKEN = os.getenv("GIT_TOKEN")
+
+
+# Load or initialize the message list
+try:
+    with open('messages.json', 'r') as file:
+        messages = json.load(file)
+except FileNotFoundError:
+    messages = []
+
+
+async def save_messages():
+    with open('messages.json', 'w') as temp_file:
+        json.dump(messages, temp_file, indent=4)
 
 
 # Asynchronous function to handle messages
 async def handle_message(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     chat = update.effective_chat
-    message_text = update.message.text.replace('\n', ' ')  # Replace newlines in messages with spaces
+    message_text = update.message.text  # .replace('\n', ' ')  # Replace newlines in messages with spaces
+    # Construct the message data as a dictionary
+    message_data = {
+        "user_id": user.id,
+        "user_name": user.first_name,
+        "chat_id": chat.id,
+        "chat_title": chat.title or 'Private Chat',
+        "message_text": message_text,
+        "timestamp": datetime.now().isoformat()
+    }
 
-    logger.info(f"Message from {user.first_name} ({user.id}) in {chat.title or 'unknown chat'}: {message_text}")
+    # Append message to the in-memory list
+    messages.append(message_data)
+    logger.info(f"Message received and stored: {message_data}")
 
-    # Save message to a file
-    with open('group_messages.txt', 'a', encoding='utf-8') as file:
-        file.write(f"{chat.id} - {chat.title or 'Private Chat'} - {user.id} - {user.first_name}: {message_text}\n")
+    await save_messages()
+
 
 # Asynchronous function to handle the /start command
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('Hi! Send me a message and I will log it.')
 
+
 # Main function to create the bot
 def main() -> None:
-    application = Application.builder().token(TOKEN).build()
+    application = Application.builder().token(GIT_TOKEN).build()
 
     # Add handlers
     application.add_handler(CommandHandler('start', start))
@@ -36,6 +64,7 @@ def main() -> None:
 
     # Run the bot until you press Ctrl-C
     application.run_polling()
+
 
 if __name__ == '__main__':
     main()
